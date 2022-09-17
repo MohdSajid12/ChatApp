e39ea3ed587bfb995c9748a281ff5f6badac63c4
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-09-17 20:52:55 +0530

    Chat backend and Get  messages With SetInterval

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ let bodyParser = require("body-parser");
 let bcrypt = require("bcrypt");
 let cors = require("cors");
 const sequelize = require("./models/database");
-
-
-
 let app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
-const signlogin=require('./routes/loginsignup');
+const signlogin=require('./routes/usersignup');
 const User=require('./models/user');
-
-
+const Message=require('./models/messages');
+const messageroute=require('./routes/message');
 app.use(signlogin);
+app.use(messageroute);
 
+User.hasMany(Message);
+Message.belongsTo(User);
 sequelize
   .sync()
   .then((result) => {
-    app.listen(3000, () => {
+    app.listen(5555, () => {
       console.log(" listening to 5555 port ");
     });
   })
   .catch((err) => {
     console.log(err);
   });
+ 
--- models/user.js
+++ models/user.js
@@ -1,32 +1,35 @@
-let Sequelize = require("sequelize");
+let Sequelize=require('sequelize')
 
-let sequelize = require("./database");
+let sequelize=require('./database');
 
-let User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  phonenumber: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
+
+let User=sequelize.define('user',{
+
+    id:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    name:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    email:{
+        type:Sequelize.STRING,
+        allowNull:false,
+        unique:true
+    }
+    ,
+    phonenumber:{
+        type:Sequelize.STRING,
+        allowNull:false,
+        unique:true
+    },
+    password:{
+        type:Sequelize.STRING,
+        allowNull:false
+    }
 });
 
-module.exports = User;
+module.exports=User;
